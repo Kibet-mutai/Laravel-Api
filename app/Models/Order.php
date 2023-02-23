771ab5f06ad0290b63038e90bf9c101e068a17d8
Kibet Mutai <kibetem24@gmail.com>
2023-02-23 11:34:38 +0300

    Add orders model

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+        'phone_no',
+        'email',
+        'zipcode',
+        'sub_county',
+        'county',
+    ];
+}
---
+++ database/migrations/2023_02_23_082329_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_no');
+            $table->string('email');
+            $table->string('zipcode');
+            $table->string('sub_county');
+            $table->string('county');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
