176e436eeb24474335594da796675072626680f1
Kibet <Kibet-mutai>
2023-02-27 19:04:05 +0300

    Create store migration

---
+++ app/Models/Store.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description'
+    ];
+
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
+}
